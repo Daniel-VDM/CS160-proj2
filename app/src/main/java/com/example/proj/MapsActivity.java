80cc7b81dd13ec44b8d03d7857ebf51a5eed91a3
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2019-10-29 20:47:37 -0700

    Skeleton on main map.

--- app/src/main/java/com/example/proj/MapsActivity.java
+++ app/src/main/java/com/example/proj/MapsActivity.java
@@ -1,29 +1,45 @@
 package com.example.proj;
 
-import androidx.fragment.app.FragmentActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     LatLng currLoc;
     String carSelected;
     String zipcode;
-    private GoogleMap mMap;
+    Button searchButton;
+    EditText zipcodeInput;
+    MapView map;
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        map = findViewById(R.id.map);
+        searchButton = findViewById(R.id.maps_searchButton);
+        zipcodeInput = findViewById(R.id.maps_zipcode);
+
+        processIntent();
+
+        map.onCreate(savedInstanceState);
+        map.getMapAsync(this);
+    }
+
+    private void processIntent(){
         Intent intent = getIntent();
         carSelected = intent.getStringExtra("CAR");
         zipcode = intent.getStringExtra("TARGET_ZIP");
@@ -32,30 +48,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // TODO: handle logic for looking things up based on zipcode (get zip here!!!)
         currLoc = new LatLng(lat, lng);
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        map.onResume();
+    }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        map.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        map.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        map.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        map.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        map.onLowMemory();
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        googleMap.setMinZoomPreference(13);
-        mMap.addMarker(new MarkerOptions().position(currLoc).title("You are here!"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(currLoc));
+        this.googleMap = googleMap;
+        googleMap.setMinZoomPreference(12);
+        googleMap.addMarker(new MarkerOptions().position(currLoc).title("You are here!"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(currLoc));
     }
+
 }
