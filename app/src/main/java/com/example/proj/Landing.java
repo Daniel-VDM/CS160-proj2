70c7f8d3c5afd03d6b5393b444d479073bd79d08
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2019-10-29 19:44:40 -0700

    Welcome page done.

--- app/src/main/java/com/example/proj/Landing.java
+++ app/src/main/java/com/example/proj/Landing.java
@@ -2,20 +2,38 @@ package com.example.proj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.nabinbhandari.android.permissions.PermissionHandler;
+import com.nabinbhandari.android.permissions.Permissions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class Landing extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -26,11 +44,15 @@ public class Landing extends AppCompatActivity implements OnMapReadyCallback {
     RadioButton modelS;
     RadioButton i3;
     String carSelected; // 'model3' OR 'modelS' OR 'i3'
+    LatLng currLoc;
+
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        carSelected = null;
 
         searchBuitton = findViewById(R.id.searchButton);
         landingMap = findViewById(R.id.landing_mapView);
@@ -41,23 +63,64 @@ public class Landing extends AppCompatActivity implements OnMapReadyCallback {
 
         setListeners();
 
+        currLoc = new LatLng(0.7143528, -74.0059731);
+
+        String[] permissions = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+        Permissions.check(this, permissions, null, null, new PermissionHandler() {
+            @Override
+            public void onGranted() {
+                requestLocationUpdate();
+            }
+
+            @Override
+            public void onDenied(Context context, ArrayList<String> deniedPermissions) {
+                super.onDenied(context, deniedPermissions);
+            }
+        });
+
         landingMap.onCreate(savedInstanceState);
         landingMap.getMapAsync(this);
     }
 
-    private void setListeners(){
+    private void requestLocationUpdate(){
+        FusedLocationProviderClient fusedLocationClient = new FusedLocationProviderClient(this);
+        final LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            currLoc = new LatLng(location.getLatitude(), location.getLongitude());
+                            googleMap.setMinZoomPreference(13);
+                            googleMap.addMarker(new MarkerOptions().position(currLoc).title("You are here!"));
+                            googleMap.moveCamera(CameraUpdateFactory.newLatLng(currLoc));
+                        }
+                    }
+                });
+    }
+
+    private void setListeners() {
         searchBuitton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO: get zipcode (if any THEN pass it through the intent)
-                Intent mapActivity = new Intent(Landing.this, MapsActivity.class);
-                startActivity(mapActivity);
+                if (carSelected == null){
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Please select a car",
+                            Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.TOP, 0, 25);
+                    toast.show();
+                } else {
+                    // TODO: Serialize all relevent info
+                    Intent mapActivity = new Intent(Landing.this, MapsActivity.class);
+                    startActivity(mapActivity);
+                }
             }
         });
         model3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if (b){
+                if (b) {
                     model3.setChecked(true);
                     modelS.setChecked(false);
                     i3.setChecked(false);
@@ -68,7 +131,7 @@ public class Landing extends AppCompatActivity implements OnMapReadyCallback {
         modelS.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if (b){
+                if (b) {
                     modelS.setChecked(true);
                     model3.setChecked(false);
                     i3.setChecked(false);
@@ -79,7 +142,7 @@ public class Landing extends AppCompatActivity implements OnMapReadyCallback {
         i3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if (b){
+                if (b) {
                     i3.setChecked(true);
                     model3.setChecked(false);
                     modelS.setChecked(false);
@@ -106,26 +169,30 @@ public class Landing extends AppCompatActivity implements OnMapReadyCallback {
         super.onStop();
         landingMap.onStop();
     }
+
     @Override
     protected void onPause() {
         landingMap.onPause();
         super.onPause();
     }
+
     @Override
     protected void onDestroy() {
         landingMap.onDestroy();
         super.onDestroy();
     }
+
     @Override
     public void onLowMemory() {
         super.onLowMemory();
         landingMap.onLowMemory();
     }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
         googleMap.setMinZoomPreference(15);
-        LatLng ny = new LatLng(40.7143528, -74.0059731);
-        googleMap.addMarker(new MarkerOptions().position(ny).title("You are here!"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(ny));
+        googleMap.addMarker(new MarkerOptions().position(currLoc).title("You are here!"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(currLoc));
     }
 }
