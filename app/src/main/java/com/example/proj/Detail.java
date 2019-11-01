e16697704123d7f2e30450e69897e46c887ed10d
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2019-11-01 01:45:00 -0700

    Detail done

--- app/src/main/java/com/example/proj/Detail.java
+++ app/src/main/java/com/example/proj/Detail.java
@@ -6,17 +6,19 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -25,11 +27,11 @@ import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Random;
 
 public class Detail extends AppCompatActivity {
 
@@ -46,12 +48,38 @@ public class Detail extends AppCompatActivity {
     ImageView closeDetail;
     ImageView openDetail;
     ImageView openMaybeDetail;
+    ImageView imageViewAAA;
+    ImageView imageViewBBB;
+    ImageView imageViewCCC;
+    ImageView imageViewAA;
+    ImageView imageViewBB;
+    ImageView imageViewCC;
+    ImageView imageViewA;
+    ImageView imageViewB;
+    ImageView imageViewC;
     TextView Title;
     TextView addressDetail;
     TextView rating;
     TextView ratingCountDetail;
     TextView priceDetail;
     TextView driveDetail;
+    TextView phoneNumber;
+    TextView textViewA;
+    TextView textViewB;
+    TextView textViewC;
+    TextView ChargeinputA;
+    TextView ChargeinputB;
+    TextView ChargeinputC;
+    TextView queueTimeA;
+    TextView queueTimeB;
+    TextView queueTimeC;
+    TextView chargeTime;
+    Button link;
+    String buttonUrl = null;
+    Button reviewButton;
+    Button sendToMaps;
+    FloatingActionButton floatingActionButton3;
+
     List<ImageView> PriceRating;
     List<ImageView> StarFull;
     List<ImageView> StarHalf;
@@ -72,6 +100,30 @@ public class Detail extends AppCompatActivity {
         ratingCountDetail = findViewById(R.id.ratingCountDetail);
         priceDetail = findViewById(R.id.priceDetail);
         driveDetail = findViewById(R.id.driveDetail);
+        phoneNumber = findViewById(R.id.phoneNumber);
+        textViewA = findViewById(R.id.textViewA);
+        textViewB = findViewById(R.id.textViewB);
+        textViewC = findViewById(R.id.textViewC);
+        ChargeinputA = findViewById(R.id.ChargeinputA);
+        ChargeinputB = findViewById(R.id.ChargeInputB);
+        ChargeinputC = findViewById(R.id.ChargeInputC);
+        queueTimeA = findViewById(R.id.queueTimeA);
+        queueTimeB = findViewById(R.id.queueTimeB);
+        queueTimeC = findViewById(R.id.queueTimeC);
+        chargeTime = findViewById(R.id.chargTime);
+        imageViewAAA = findViewById(R.id.imageViewAAA);
+        imageViewBBB = findViewById(R.id.imageViewBBB);
+        imageViewCCC = findViewById(R.id.imageViewCCC);
+        imageViewAA = findViewById(R.id.imageViewAA);
+        imageViewBB = findViewById(R.id.imageViewBB);
+        imageViewCC = findViewById(R.id.imageViewCC);
+        imageViewA = findViewById(R.id.imageViewA);
+        imageViewB = findViewById(R.id.imageViewB);
+        imageViewC = findViewById(R.id.imageViewC);
+        link = findViewById(R.id.link);
+        reviewButton = findViewById(R.id.reviewButton);
+        floatingActionButton3 = findViewById(R.id.floatingActionButton3);
+        sendToMaps = findViewById(R.id.sendToMaps);
 
         PriceRating = new ArrayList<>();
         PriceRating.add((ImageView) findViewById(R.id.money0));
@@ -95,7 +147,8 @@ public class Detail extends AppCompatActivity {
         processIntent();
         getDetail();
         setDetailsHeader();
-        setDetailBody();
+        setDetailsBody();
+        setNextPageListeners();
     }
 
     private void processIntent() {
@@ -241,8 +294,142 @@ public class Detail extends AppCompatActivity {
         }
     }
 
-    private void setDetailBody(){
-        // TODO The logic for the body + buttons.
+    @SuppressLint("SetTextI18n")
+    private void setDetailsBody(){
+        try {
+            Random rand= new Random();
+            if (jsonObjectDetail.has("international_phone_number")){
+                phoneNumber.setText(jsonObjectDetail.getString("international_phone_number"));
+            } else if (jsonObjectDetail.has("formatted_phone_number")){
+                phoneNumber.setText(jsonObjectDetail.getString("formatted_phone_number"));
+            } else {
+                phoneNumber.setText("Unknown Phone Number");
+            }
+
+            if (jsonObjectDetail.has("website")){
+                buttonUrl = jsonObjectDetail.getString("website");
+                link.setText(buttonUrl);
+            } else {
+                link.setText("Unknown Website");
+            }
+
+            if (jsonObjectSearch.getString("name").contains("Tesla")){
+                imageViewBBB.setAlpha(0.0f);
+                imageViewCCC.setAlpha(0.0f);
+                imageViewBB.setAlpha(0.0f);
+                imageViewCC.setAlpha(0.0f);
+                imageViewB.setAlpha(0.0f);
+                imageViewC.setAlpha(0.0f);
+                textViewB.setAlpha(0.0f);
+                textViewC.setAlpha(0.0f);
+                queueTimeB.setAlpha(0.0f);
+                queueTimeC.setAlpha(0.0f);
+                ChargeinputB.setAlpha(0.0f);
+                ChargeinputC.setAlpha(0.0f);
+                textViewA.setText("Supercharger");
+                if (Math.random() > 0.5){
+                    int curr = (int)(10.0 * Math.random() + 1);
+                    ChargeinputA.setText(curr + "/11");
+                    queueTimeA.setText("No Queue!");
+                } else {
+                    int queue = (int)(rand.nextDouble() * 60);
+                    ChargeinputA.setText("0/11");
+                    queueTimeA.setText("Queue: " + queue + "min");
+                }
+            } else {
+                if (Math.random() > 0.5) {
+                    int curr = (int)(5 * rand.nextDouble()) + 1;
+                    ChargeinputA.setText(curr + "/6");
+                    queueTimeA.setText("No Queue!");
+                } else {
+                    int queue = (int)(rand.nextDouble() * 60);
+                    ChargeinputA.setText("0/6");
+                    queueTimeA.setText("Queue: " + queue + "min");
+                }
+                if (Math.random() > 0.5) {
+                    int curr = (int)(6 * rand.nextDouble() + 1);
+                    ChargeinputB.setText(curr + "/7");
+                    queueTimeB.setText("No Queue!");
+                } else {
+                    int queue = (int)(rand.nextDouble() * 60);
+                    ChargeinputB.setText("0/7");
+                    queueTimeB.setText("Queue: " + queue + "min");
+                }
+                if (Math.random() > 0.5){
+                    int curr = (int)(10.0 * Math.random() + 1);
+                    ChargeinputC.setText(curr + "/11");
+                    queueTimeC.setText("No Queue!");
+                } else {
+                    int queue = (int)(rand.nextDouble() * 60);
+                    ChargeinputC.setText("0/11");
+                    queueTimeC.setText("Queue: " + queue + "min");
+                }
+            }
+
+            int rangePerHr = jsonObjectSearch.getInt("range-per-hr-i3");
+            double leftRange = RecyclerViewAdapter.Model3Range - RecyclerViewAdapter.currModel3Range;
+            double timeLeft = new BigDecimal( leftRange / rangePerHr)
+                    .setScale(2, RoundingMode.HALF_UP).doubleValue();
+            if (timeLeft > 1){
+                chargeTime.setText("Charge: " + timeLeft + " hrs");
+            } else {
+                chargeTime.setText("Charge: " + timeLeft*60 + " mins");
+            }
+        } catch (JSONException e){
+            Log.e(TAG, e.toString());
+        }
+    }
+
+    private void setNextPageListeners(){
+        reviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent review = new Intent(Detail.this, Review.class);
+                String jsonObjectDetailedString = jsonObjectDetail.toString();
+                review.putExtra("JSON_OBJECT", jsonObjectDetailedString);
+                startActivity(review);
+            }
+        });
+
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (buttonUrl != null){
+                    Uri uri = Uri.parse(buttonUrl);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        sendToMaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Uri uri = Uri.parse(jsonObjectDetail.getString("url"));
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                } catch (JSONException ignore){
+                }
+            }
+        });
+
+        floatingActionButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    double lat = jsonObjectDetail.getJSONObject("geometry")
+                            .getJSONObject("location").getDouble("lat");
+                    double lng = jsonObjectDetail.getJSONObject("geometry")
+                            .getJSONObject("location").getDouble("lng");
+                    Uri gmmIntentUri = Uri.parse("google.streetview:cbll=" + lat + "," + lng);
+                    Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                    mapIntent.setPackage("com.google.android.apps.maps");
+                    startActivity(mapIntent);
+                } catch (JSONException ignore){
+                }
+            }
+        });
     }
 
     private String read(String httpUrl) {
