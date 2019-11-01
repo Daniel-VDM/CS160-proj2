4069b9d70804b3c637936bd3d7ebffc717d30ca0
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2019-10-31 23:15:49 -0700

    Review front-end is done.

---
+++ app/src/main/java/com/example/proj/Review.java
@@ -0,0 +1,16 @@
+package com.example.proj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Review extends AppCompatActivity {
+
+    // TODO: Process intent and update things... (No real logic is done).
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review);
+    }
+}
